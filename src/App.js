3f686cd4c8976acc10429b7bbfec3affece228f0
Andy2020lv <andyhernandez5102@gmail.com>
2022-12-16 11:01:50 -0400

    Identified some limitations. The max end daqte I can use is 2021 because of the EARTH api not finding locations ahead of that date. On the other hand, the EONET api can only retrieve data prior to 2022 when the category is wildfires or volacons, otherwise all the other categories are on 2022.

--- src/App.js
+++ src/App.js
@@ -5,26 +5,28 @@ import React from "react";
 import Events from "./components/Events";
 import Event from "./components/Event";
 
-const EONET_API_URL =
-  "https://eonet.gsfc.nasa.gov/api/v3/events?start=2019-01-01&end=2021-12-31&limit=5&key=";
-const NASA_API_KEY = "ijz7SNQHjWKEmWblGRlmfPq3nCPhg6LuCNyjZcgb";
 function App() {
+  const [category, setCategory] = React.useState("");
   const [nasaData, setNasaData] = React.useState(null);
+
+  const EONET_API_URL = `https://eonet.gsfc.nasa.gov/api/v3/events?start=2010-01-01&end=2021-12-31&limit=5&category=${category}&key=`;
+  const NASA_API_KEY = "ijz7SNQHjWKEmWblGRlmfPq3nCPhg6LuCNyjZcgb";
 
   React.useEffect(() => {
     fetch(EONET_API_URL + NASA_API_KEY)
       .then((response) => response.json())
       .then((data) => setNasaData(data))
       .catch((error) => console.log(error));
-  }, []);
+  }, [category]);
 
   if (!nasaData) {
     return <p>Loading...</p>;
   }
 
-  console.log(nasaData);
+  // console.log(nasaData);
   let nasaElements = nasaData.events.map((element) => (
     <Events
+      category={category}
       coordinates={element.geometry[0].coordinates}
       id={element.id}
       date={element.geometry[0].date}
@@ -32,6 +34,34 @@ function App() {
   ));
   return (
     <div>
+      <div class="btn-group">
+        <button
+          class="btn btn-secondary dropdown-toggle"
+          type="button"
+          data-bs-toggle="dropdown"
+          data-bs-auto-close="true"
+          aria-expanded="false"
+        >
+          Categories
+        </button>
+        <ul class="dropdown-menu">
+          <li>
+            <a class="dropdown-item" onClick={() => setCategory("wildfires")}>
+              Wildfire
+            </a>
+          </li>
+          <li>
+            <a class="dropdown-item" onClick={() => setCategory("manmade")}>
+              manmade
+            </a>
+          </li>
+          <li>
+            <a class="dropdown-item" onClick={() => setCategory("volcanoes")}>
+              Volcanoes
+            </a>
+          </li>
+        </ul>
+      </div>
       <nav>
         <ul>
           <li>
--- src/components/Events.js
+++ src/components/Events.js
@@ -6,33 +6,37 @@ export default function Events(props) {
   let [lon, lat] = props.coordinates.map((x) => x);
   const date = props.date.slice(0, 10);
 
-  const EARTH_API_URL = `https://api.nasa.gov/planetary/earth/assets?lon=${lon}&lat=${lat}&date=${date}&dim=0.25&api_key=`;
+  const EARTH_API_URL = `https://api.nasa.gov/planetary/earth/assets?lon=${lon}&lat=${lat}&date=${date}&dim=0.15&&api_key=`;
   const NASA_API_KEY = "ijz7SNQHjWKEmWblGRlmfPq3nCPhg6LuCNyjZcgb";
-  //   const regex = /(\d{4})-(\d{2})-(\d{2})/;
-  //   const regex = /\d{4}-\d{2}-\d{2}/;
 
+  const [src, setSrc] = React.useState();
   const [earthData, setEarthData] = React.useState(null);
 
   React.useEffect(() => {
     fetch(EARTH_API_URL + NASA_API_KEY)
       .then((response) => response.json())
       .then((data) => setEarthData(data))
+      // .then((earthData) => setSrc(earthData.url))
       .catch((error) => console.log(error));
-  }, []);
+  }, [lon]);
   if (!earthData) {
     return <p>Loading...</p>;
   }
 
   //   alert(earthData.url);
-
   <Event id={props.id} lon={lon} lat={lat} />;
+  console.log(earthData.id);
+  // const Img = React.memo(() => <img src={earthData.url} />);
+  // const image = React.useImage()
 
   return (
     <div>
       <h1>{props.id}</h1>
       <p>{lon}</p>
       <p>{lat}</p>
-      <img className="sateliteImg" alt="" src={earthData.url}></img>
+
+      <img alt="" className="sateliteImg" src={`${earthData.url}`}></img>
+
       <p>{date}</p>
       {/* <p>{props.lon}</p> */}
       <nav>
